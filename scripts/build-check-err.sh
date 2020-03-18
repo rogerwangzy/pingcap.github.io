34d772c2d3edc650ef99b38aebdd21164ca0f7d1
Yue Yang <g1enyy0ung@gmail.com>
2020-03-18 17:32:26 +0800

    fix: ci timeout

--- scripts/build-check-err.sh
+++ scripts/build-check-err.sh
@@ -2,7 +2,7 @@ gulp build > gulp-build.log
 
 if cat gulp-build.log | grep -i '^error'
 then
-  echo 'Found Error in build.log. Exit'
+  echo 'Found Error in build.log. Exit.'
   exit -1
 else
    bash ./scripts/process-docs.sh
--- scripts/gen-nav.js
+++ scripts/gen-nav.js
@@ -96,4 +96,6 @@ if (args.length != 3) {
     'Usage: node <this> arg1 arg2 arg3 # arg1 markdown source path, arg2 json file path arg3 url prefix'
   )
 }
+
+console.log(`Start to genTableJSONFromMarkdown: ${args[2]}`)
 genTableJSONFromMarkdown(args[0], args[1], args[2])
